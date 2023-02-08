d38b901fa737a1958a4fe93e1ee60b27308a9595
Jimmy Yao <jimmyyaocs@gmail.com>
2023-02-08 16:39:53 +1100

    Update package.json and Sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -115,7 +115,7 @@ const Sidebar = ({ user, drawerWidth, isSidebarOpen, setIsSidebarOpen, isNonMobi
               <FlexBetween color={theme.palette.secondary.main}>
                 <Box display="flex" alignItems="center" gap="0.5rem">
                   <Typography variant="h4" fontWeight="bold">
-                    ECOMVISION
+                    ADMINHUB
                   </Typography>
                 </Box>
                 {!isNonMobile && (
